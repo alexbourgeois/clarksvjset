63a052c99eaa3af70e4c7346b9cf4913974cdddb
Alex Bourgeois <abourgeois27380@gmail.com>
2019-07-25 19:28:36 +0200

    Added strobe effect

---
+++ Assets/Scripts/CameraFadeToColor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFadeToColor : MonoBehaviour {
+
+    [Range(0,1)]
+    public float FadeValue = 0.0f;
+    public Color FadeColor = Color.black;
+
+    private Material _fadingMaterial;
+
+    private void OnValidate()
+    {
+        if (_fadingMaterial)
+            ChangeFadeColor(FadeColor);
+    }
+
+    private void Awake()
+    {
+        _fadingMaterial = new Material(Shader.Find("Unlit/FadeToColor"));
+        _fadingMaterial.SetVector("_FadingColor", FadeColor);
+        _fadingMaterial.SetFloat("_FadingPercent", FadeValue);
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        _fadingMaterial.SetFloat("_FadingPercent", 1.0f - FadeValue);
+
+        Graphics.Blit(source, destination, _fadingMaterial);
+    }
+
+    public void ChangeFadeColor(Color newColor)
+    {
+        FadeColor = newColor;
+        _fadingMaterial.SetVector("_FadingColor", FadeColor);
+    }
+}
---
+++ Assets/Scripts/Strobe.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Strobe : MonoBehaviour
+{
+    public CameraFadeToColor fadeToColor;
+
+    public float duration;
+    public AnimationCurve curve;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        //fadeToColor.FadeColor = Color.HSVToRGB(UnityEngine.Random.value, 1.0f, 1.0f);
+        //fadeToColor.ChangeFadeColor(fadeToColor.FadeColor);
+
+        StopAllCoroutines();
+        StartCoroutine(fade(0.0f, 1.0f, duration, curve, Callback));
+    }
+
+    private void OnDisable()
+    {
+        fadeToColor.FadeValue = 0.0f;
+    }
+
+    void Callback()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public IEnumerator fade(float start, float end, float duration, AnimationCurve curve, Action callback = null)
+    {
+        var currentTime = 0.0f;
+        while (currentTime / duration < 1)
+        {
+            fadeToColor.FadeValue= Mathf.Lerp(start, end, curve.Evaluate(currentTime / duration)); ;
+            currentTime += Time.deltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+
+        callback?.Invoke();
+    }
+}
